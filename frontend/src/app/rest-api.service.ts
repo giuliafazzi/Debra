60c2e72a6d5b42db5311334e37812033af9c577e
Felipe Guimarães <fe_lfg@hotmail.com>
2019-11-18 19:56:54 -0300

    aaa

--- frontend/src/app/rest-api.service.ts
+++ frontend/src/app/rest-api.service.ts
@@ -6,7 +6,7 @@ import { catchError, tap, map } from 'rxjs/operators';
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
-const apiUrl = "http://0.0.0.0:3331";
+const apiUrl = "http://localhost:3000";
 
 @Injectable({
   providedIn: 'root'
