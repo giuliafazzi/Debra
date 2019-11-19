82bee4b8a8485888f05a2f3a080ed8e613851c97
giulia <giuliafazzi@hotmail.com>
2019-11-19 12:34:51 -0300

    alinhamento botão + melhorias no front

--- frontend/src/app/publicacao/publicacao.page.ts
+++ frontend/src/app/publicacao/publicacao.page.ts
@@ -1,37 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-import { RestApiService } from '../rest-api.service';
-import { ActivatedRoute, Router  } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
+import { RestApiService } from "../rest-api.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-publicacao',
-  templateUrl: './publicacao.page.html',
-  styleUrls: ['./publicacao.page.scss'],
+  selector: "app-publicacao",
+  templateUrl: "./publicacao.page.html",
+  styleUrls: ["./publicacao.page.scss"]
 })
 export class PublicacaoPage implements OnInit {
-  aluno_id: string = this.route.snapshot.paramMap.get('aluno_id');
-  escola_id: string = this.route.snapshot.paramMap.get('escola_id');
-  categoria: string = this.route.snapshot.queryParamMap.get('categoria');
+  aluno_id: string = this.route.snapshot.paramMap.get("aluno_id");
+  escola_id: string = this.route.snapshot.paramMap.get("escola_id");
+  categoria: string = this.route.snapshot.queryParamMap.get("categoria");
   data: any = {
-    content:'',
+    content: "",
     category: this.categoria
   };
 
-  constructor(public api: RestApiService,
+  constructor(
+    public api: RestApiService,
     public loadingController: LoadingController,
     private route: ActivatedRoute,
-    public router: Router) {}
+    public router: Router
+  ) {}
 
-    async createPost() {
-      console.log(this.data);
-      await this.api.createPost(this.data, this.escola_id, this.aluno_id)
-        .subscribe(res => {
-          this.router.navigate([`/escolas/${this.escola_id}/alunos/${this.aluno_id}/evolucao`]);
-        }, err => {
+  async createPost() {
+    console.log(this.data);
+    await this.api
+      .createPost(this.data, this.escola_id, this.aluno_id)
+      .subscribe(
+        res => {
+          this.router.navigate([
+            `/escolas/${this.escola_id}/alunos/${this.aluno_id}/evolucao`
+          ]);
+        },
+        err => {
           console.log(err);
-        });
-    }
-
-  ngOnInit() {
+        }
+      );
   }
+
+  ngOnInit() {}
 }
